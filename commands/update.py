64a014b1beca54cd8b28d29a7638467a3890095b
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2020-11-30 02:51:52 +0800

    add update function

---
+++ commands/update.py
@@ -0,0 +1,40 @@
+import json
+import os
+#import requests
+from getpass import getpass
+
+from constants import COOKIES_DIR
+from constants import ASSIGNMENT_MAPPING_PATH
+from util.common import get_csrf_token
+from util.curl import curl
+
+def update_map():
+	endpoint = "contests?offsets=0&limit=100&status=0"
+	payload = {
+			"offsets":0,
+			"limit":100,
+			"status":0
+	}
+	inputstr = '{'
+	result = json.loads(curl("get", payload=payload, endpoint=endpoint, use_x_csrf_token=True))
+	for i in range(0,len(result['data']['results'])):
+		contestid = result['data']['results'][i]['id']
+		payload = {
+				"contest_id" : str(contestid)
+				}
+		endpoint = "contest/problem?contest_id=" + str(contestid)
+		result2 = json.loads(curl("get", payload=payload, endpoint=endpoint, use_x_csrf_token=True))
+		q_string = result2["data"][0]["_id"]
+		_pid = q_string.split()[0] + "+" + q_string.split()[1]
+		print("Found HomeWork: " + q_string.split()[1])
+		if i != 0:
+			inputstr += ','
+		inputstr += '"' + str(q_string.split()[1])+'":{"contest_id":' + str(contestid) + ',"contest_problem_id":"' + str(_pid)+ '","problem_id":' + str(result2["data"][0]["id"]) + '}'
+	inputstr += '}'
+	f = open(ASSIGNMENT_MAPPING_PATH,'w')
+	f.write(inputstr)
+	f.close
+	print("Updated successfully!")
+		
+
+		
