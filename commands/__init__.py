2f04c9934fbf868d19710e2be745807a86ee60f0
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2020-11-30 02:51:18 +0800

    add update function

---
+++ commands/__init__.py
@@ -0,0 +1,8 @@
+from .get_assign import get_assign
+from .get_problem import get_problem
+from .login import auth
+from .status import status
+from .submit import submit
+from .update import update_map
+
+__all__ = ["auth", "get_assign", "submit", "get_problem", "status", "update_map"]
