c827638a714f8faf25f1322d29aa5e1897295489
andyjjrt <andyjjrt@gmail.com>
2020-12-11 16:52:56 +0800

    fix bug: can't show TLE properly.

--- commands/status.py
+++ commands/status.py
@@ -15,26 +15,26 @@ def status(submission_id):
 	if response_data == "Submission doesn't exist":
 		print("Submission doesn't exist")
 		return
-	print("=================================================")
+	print("========================================================")
 	status_to_response = {
 			-1: red_wrapper("WA(Wrong Answer)"),  # WA
 			-2: cyan_wrapper("CE(Compilation Error)"),  # CE
 			0: green_wrapper("AC(Accept)"),  # AC
-			2: "TLE(Time Limit Exceeded)",  # TLE
-			3: "MLE(Memory Limit Exceeded)",  # MLE
+			1: purple_wrapper("TLE(TimeLimitExceeded)"),  # TLE
+			3: purple_wrapper("MLE(MemoryLimitExceeded)"),  # MLE
 			4: purple_wrapper("RE(Runtime Error)"),  # RE
 			8: cyan_wrapper("PAC(Partial Accepted)")
 			}
 	this_result = response_data["result"]
 	try:
-		print("Result: {:33}  Score:{:4}\n".format(status_to_response[this_result],response_data["statistic_info"]["score"]))
+		print("Result: {:40}  Score:{:4}\n".format(status_to_response[this_result],response_data["statistic_info"]["score"]))
 		if "data" not in response_data["info"]:
 			print(gray_wrapper(response_data["statistic_info"]["err_info"]))
-			print("=================================================")
+			print("========================================================")
 			return
-		print("|ID |Status                |   Time|  Mem| Score|")
+		print("|ID |Status                       |   Time|  Mem| Score|")
 		for ans in result["data"]["info"]["data"]:
-			print("|#{:2}|{:33}|{:5}ms|{:3}MB| {:5}|".format(ans["test_case"],status_to_response[ans["result"]], ans["real_time"], (ans["memory"]/1048576)+1, ans["score"]))
-		print("=================================================")
+			print("|#{:2}|{:40}|{:5}ms|{:3}MB| {:5}|".format(ans["test_case"],status_to_response[ans["result"]], ans["real_time"], (ans["memory"]/1048576)+1, ans["score"]))
+		print("========================================================")
 	except:
 		print("Bad response! Please check your internet connection and ask TAs!")
