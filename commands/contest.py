af7ea50cf7cec50b8cc6b10d499780c3e759a633
andyjjrt <andyjjrt@gmail.com>
2020-12-16 11:56:26 +0800

    Fixed error

--- commands/contest.py
+++ commands/contest.py
@@ -165,7 +165,7 @@ def contests_result(assign_name):
 			8: cyan_wrapper("PAC(Partial Accepted)")
 			}
 	if result2["my_status"] == None:
-		print("Your status of " + assign_to_config[assign_name]["contest_name"] + " : No rrecord")
+		print("Your status of " + assign_to_config[assign_name]["contest_name"] + " : No record")
 	else:
 		print("Your status of " + assign_to_config[assign_name]['contest_name'] + " : " + status_to_response[result2["my_status"]])
 	print("================================================")
